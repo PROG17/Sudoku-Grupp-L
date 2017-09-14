a0b0e8c53adad2740874679d79adbae9bfea5861
uppstyrt <hampus.boesen@gmail.com>
2017-09-14 11:31:55 +0200

    Skapat bräde + parsat värden + printfunktion

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace Sudoku_Grupp_L
     {
         static void Main(string[] args)
         {
+            Sudoku game = new Sudoku("003020600900305001001806400" +
+                                     "008102900700000008006708200" +
+                                     "002609500800203009005010300");
+
+            game.PrintToScreen();
+
+            /*
+            game.Solve();
+            Console.WriteLine(game.BoardAsText);
+            */
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Sudoku.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Grupp_L
+{
+
+    class Sudoku
+    {
+        int[,] gameBoard = new int [9,9];
+
+
+        public Sudoku(string numbersInput)
+        {
+            for (int i = 0; i < numbersInput.Length; i++)
+            {
+                char number = numbersInput[i];
+                int.TryParse(number.ToString(), out int parsedNumber);
+                int x = i % 9;
+                int y = i / 9;
+                this.gameBoard[x, y] = parsedNumber;
+
+            }
+            
+        }
+
+        public void PrintToScreen()
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    if (this.gameBoard[x,y] == 0)
+                    {
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.Write(this.gameBoard[x, y] + " ");
+
+                    }
+
+                    if ((x % 3 == 2) && (x != 8))
+                    {
+                        Console.Write("| ");
+                    }
+
+                }
+
+                Console.WriteLine();
+
+                if ((y % 3 == 2) && (y != 8))
+                {
+                    Console.WriteLine("---------------------");
+
+                }
+
+            }
+        }
+    }
+}
